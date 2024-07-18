7047cb47fe67632738f846aa4434d5d8cf1aa1df
Shubham Hatzade <71142023+Shubhamhatzade15@users.noreply.github.com>
2024-07-18 13:32:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import Footer from '@/components/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Travlog App',
-  description: 'Travel UI/UX App',
+  title: 'Travlog',
+  description: 'Shubham Hatzade',
 }
 
 export default function RootLayout({
